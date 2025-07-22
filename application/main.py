dcb9a95dfeec727acb63a2e4a1ee5c905849d128
Cyconekitten <157323498+DevCyclonekittenTriHex@users.noreply.github.com>
2025-07-22 15:43:44 +1000

    e
    e

--- application/main.py
+++ application/main.py
@@ -1,6 +1,29 @@
-import autonomous as auto
-def RunFunction(string):
-    if(string=="pX"):
-        asyncio.Run(auto.MovePrinterX())
+import cherrypy
+import asyncio
+
+class MyApp:
+    @cherrypy.expose
+    def index(self):
+        return open('site\\index.html')
 
     
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def run(self):
+        try:
+            input_string = cherrypy.request.json.get('input_string', '')
+            result = e()
+            print(result)
+            return {'result': result}
+        except Exception as e:
+            return {'error': str(e)}
+
+
+    def e():
+        print("HI")
+        return "HI"
+
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(MyApp())
